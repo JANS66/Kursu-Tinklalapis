298c5e2954cc265ddbfe12d5d87bd4171052d01d
JANS <JANS66@protonmail.com>
2023-04-01 11:51:21 +0300

    Login form styled

--- kursu-tinklalapis-frontend/src/components/LoginForm.js
+++ kursu-tinklalapis-frontend/src/components/LoginForm.js
@@ -1,42 +1,92 @@
 import React, { useState } from 'react';
+import './LoginForm.css';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        try {
+
+        let hasError = false;
+
+        if (!email.trim()) {
+          setEmailError('El. paštas negali būti tuščias');
+          hasError = true;
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+          setEmailError('Netinkamas el. pašto formatas');
+          hasError = true;
+        } else {
+          setEmailError('');
+        }
+
+        if (!password.trim()) {
+          setPasswordError('Slaptažodis negali būti tuščias');
+          hasError = true;
+        } else if (password.length < 8) {
+          setPasswordError('Slaptažodis turi būti bent 8 simbolių ilgio');
+          hasError = true;
+        } else {
+          setPasswordError('');
+        }
+
+        if (!hasError) {
+          try {
             const response = await fetch('/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ email, password })
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ email, password })
             });
+
             if (response.ok) {
-                window.location.href = '/';
+              window.location.href = '/';
             } else {
-                alert('Klaidingas el pastas ir slaptazodis');
+              alert('Klaidingas el pastas ir slaptazodis');
             }
-        }   catch (error) {
+          } catch (error) {
             console.error(error);
             alert('Ivyko klaida');
+          }
         }
-    };
+      };
 
-    return (
-        <form onSubmit={handleLogin}>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      const inputErrorStyle = {
+        border: '1px solid red',
+        outlineColor: 'red',
+      };
+
+      return (
+        <div className="RegistrationForm">
+          <form onSubmit={handleLogin}>
+            <div>
+              <label htmlFor="email">El. Paštas:</label>
+              <input 
+                type="text" 
+                id="email" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)}
+                style={emailError ? inputErrorStyle : null}
+              />
+              {emailError && <span className="error">{emailError}</span>}
           </div>
           <div>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <label htmlFor="password">Slaptažodis:</label>
+            <input 
+              type="password" 
+              id="password" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+              style={passwordError ? inputErrorStyle : null}
+            />
+            {passwordError && <span className="error">{passwordError}</span>}
           </div>
-          <button type="submit">Log In</button>
+          <button type="submit">Prisijungti</button>
         </form>
+      </div>
       );
     }
     
--- kursu-tinklalapis-frontend/src/components/RegistrationForm.js
+++ kursu-tinklalapis-frontend/src/components/RegistrationForm.js
@@ -13,6 +13,7 @@ function RegistrationForm() {
   const [lastNameError, setLastNameError] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const [confirmPasswordError, setConfirmPasswordError] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -54,9 +55,11 @@ function RegistrationForm() {
   
     if (password !== confirmPassword) {
       setPasswordsMatch(false);
+      setConfirmPasswordError('Slaptažodžiai nesutampa')
       hasError = true;
     } else {
       setPasswordsMatch(true);
+      setConfirmPasswordError('');
     }
   
     if (!hasError) {
@@ -71,44 +74,74 @@ function RegistrationForm() {
     }
   };
 
-  const confirmPassStyle = {
-    border: passwordsMatch ? 'none' : '1px solid red'
-  }
-
   const inputErrorStyle = {
     border: '1px solid red',
     outlineColor: 'red',
-  }
+  };
+
+  const confirmPassStyle = passwordsMatch ? {} : { border: '1px solid red'};
 
   return (
     <div className="RegistrationForm">
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="firstName">Vardas:</label>
-          <input type="text" id="firstName" value={firstName} onChange={(event) => setFirstName(event.target.value)} style={firstNameError ? inputErrorStyle : null} />
+          <input
+            type="text"
+            id="firstName"
+            value={firstName}
+            onChange={(event) => setFirstName(event.target.value)}
+            style={firstNameError ? inputErrorStyle : null}
+          />
           {firstNameError && <span className="error">{firstNameError}</span>}
         </div>
         <div>
           <label htmlFor="lastName">Pavardė:</label>
-          <input type="text" id="lastName" value={lastName} onChange={(event) => setLastName(event.target.value)} style={lastNameError ? inputErrorStyle : null} />
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={(event) => setLastName(event.target.value)}
+            style={lastNameError ? inputErrorStyle : null}
+          />
           {lastNameError && <span className="error">{lastNameError}</span>}
         </div>
         <div>
           <label htmlFor="email">El. Paštas:</label>
-          <input type="email" id="email" value={email} onChange={(event) => setEmail(event.target.value)} style={emailError ? inputErrorStyle : null} />
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            style={emailError ? inputErrorStyle : null}
+          />
           {emailError && <span className="error">{emailError}</span>}
         </div>
         <div>
           <label htmlFor="password">Slaptažodis:</label>
-          <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} style={passwordError ? inputErrorStyle : null} />
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            style={passwordError ? inputErrorStyle : null}
+          />
           {passwordError && <span className="error">{passwordError}</span>}
         </div>
         <div>
           <label htmlFor="confirmPassword">Pakartokite Slaptažodį:</label>
-          <input type="password" id="confirmPassword" value={confirmPassword} onChange={(event) => setConfirmPassword(event.target.value)} style={confirmPassStyle} />
-          {!passwordsMatch && <span className="error">Slaptažodžiai nesutampa</span>}
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+            style={confirmPasswordError ? inputErrorStyle : null}
+          />
+          {!passwordsMatch && (
+            <span className="error">Slaptažodžiai nesutampa</span>
+          )}
         </div>
-          <button type="submit">Registruotis</button>
+        <button type="submit">Registruotis</button>
       </form>
     </div>
   );
